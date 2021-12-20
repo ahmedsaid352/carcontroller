1beb2e9723d58dae09dd6a2c94304849c83c244f
unknown <ahmedsaid2000321@gmail.com>
2021-12-20 16:36:43 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+
+const PORT = 80
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+"/views/index.html");
+});
+
+const server = app.listen(PORT,function(){
+    console.log("Server is runinng at port "+PORT);
+});
+
+const SocketServer = require('ws').Server;
+const wss = new SocketServer({ server });
+
+
+
+wss.on('connection', function(ws){
+	console.log('Client connected');
+
+	ws.on('message', function(msg){
+		myjson = JSON.parse(msg);
+		broadcast(myjson);
+		console.log(myjson);
+	});
+	
+	ws.on('close', function(){
+		console.log('Client disconnected');
+	});
+});
+
+function broadcast(msg) {
+	wss.clients.forEach(function (client) {
+		if (client.readyState === client.OPEN) { // if client still connected
+			client.send(msg);
+		}else{
+			console.log("client off");
+		}
+	});
+}
